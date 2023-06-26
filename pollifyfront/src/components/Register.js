d3fc5910b5dfd85b776d451246f0f4b9b9868533
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2023-06-26 18:28:22 +0530

    Update Register.js

--- pollifyfront/src/components/Register.js
+++ pollifyfront/src/components/Register.js
@@ -146,7 +146,7 @@ export default function Register()
     if(validation())
     {
 
-        axios.post(`http://localhost:5000/api/auth/register`, register)
+        axios.post(`https://pollify-backend.onrender.com/api/auth/register`, register)
         .then( res => {
             // alert(res.data);
             swal({
